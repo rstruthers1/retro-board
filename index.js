cfbb6c8e15e544e7f9aadf3c25e347a04f185ff6
rstruthers1 <rstruthers1@yahoo.com>
2016-09-04 08:45:38 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Rachel on 9/4/2016.
+ */
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});

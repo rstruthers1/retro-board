881c13795d8902859761b0dec40b220eea252a14
rstruthers1 <rstruthers1@yahoo.com>
2016-09-04 09:17:11 -0500

    try papertrail

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ var port = process.env.PORT || 8080;
 
 
 app.get('/', function (req, res) {
-    res.send('Hello World! I was deployed from github! Test autodeploy');
+    res.send('Hello World!');
 });
 
 app.listen(port, function () {
-    console.log('Example app listening on port 8080!');
+    console.log('Example app listening on port ' + port + '!');
 });

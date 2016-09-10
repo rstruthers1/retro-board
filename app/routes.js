0efdb8115f37bc3b12d069d7ac46e6c1dfe3e537
rstruthers1 <rstruthers1@yahoo.com>
2016-09-10 07:57:23 -0500

    Start of profile page. Put user's username at top right if logged in.

--- app/routes.js
+++ app/routes.js
@@ -60,4 +60,24 @@ module.exports = function(app, passport) {
         failureFlash : true // allow flash messages
     }));
 
+    // =====================================
+    // PROFILE ==============================
+    // =====================================
+
+    app.get('/profile', isLoggedIn, function(req, res) {
+
+        res.render('pages/profile.ejs');
+    });
+
+    // route middleware to make sure a user is logged in
+    function isLoggedIn(req, res, next) {
+
+        // if user is authenticated in the session, carry on
+        if (req.isAuthenticated())
+            return next();
+
+        // if they aren't redirect them to the home page
+        res.redirect('/');
+    }
+
 }
